fc89dacadf7c78494b170c65b19dd030b388ae7a
Youness KAFIA <32330908+ykafia@users.noreply.github.com>
2023-06-12 20:48:47 +0200

    added mixin data

--- src/SoftTouch.Spirv/Mixin.cs
+++ src/SoftTouch.Spirv/Mixin.cs
@@ -3,6 +3,14 @@ namespace SoftTouch.Spirv;
 
 public class Mixin : BaseMixin
 {
+    public List<Mixin> Mixins;
     public List<VariableData> Variables;
     public List<MethodData> Methods;
+
+    public Mixin()
+    {
+        Mixins = new();
+        Variables = new();
+        Methods = new();
+    }
 }
